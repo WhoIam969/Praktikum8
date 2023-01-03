c98329fa70d903f7cfe0deb7209a6d6593e3ba66
WhoIam969 <rizkiberkah107@gmail.com>
2023-01-03 20:37:11 +0700

    Program

---
+++ Order.java
@@ -0,0 +1,24 @@
+package Praktikum8;
+
+public class Order {
+    String date;
+	String status;
+	public OrderDetail detail;
+
+	public Order(OrderDetail e) {
+		detail = e;
+	}
+
+	public void calcSubTotal() {
+		detail.calcSubTotal();
+	}
+	public void calcTax() {
+		detail.calcTax();
+	}
+	public void calcTotal() {
+		System.out.println("Kalkulasi total semua");
+	}
+	public void calcTotalWeight() {
+		detail.calcTotalWeight();
+	}
+}
