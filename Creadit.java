f5e9c42be043d1e27e89b195599d8882f97dbca3
WhoIam969 <rizkiberkah107@gmail.com>
2023-01-03 20:31:42 +0700

    Program

---
+++ Creadit.java
@@ -0,0 +1,11 @@
+package Praktikum8;
+
+public class Creadit {
+    String number;
+    String type;
+    int expDate;
+
+    public void authorized() {
+        System.out.println("Verifikasi");
+    }
+}
